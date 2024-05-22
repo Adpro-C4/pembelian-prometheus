f0b48dbb8f723888f80ddb89090f0a291abad4ff
isaui <isadestroyed17@gmail.com>
2024-05-22 19:00:12 +0700

    aaa

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
- docker-compose up --build -d   
+#!/bin/bash
+cd prometheus
+# Build Docker image
+docker build -t nama_image:tag .
+# Run Docker container
+docker run -d -p 9090:9090 --name prometheus_container nama_image:tag
+   
